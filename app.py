b76b2599fd9f638df2893f54cef6c3bb2c6742ee
Anthony <sean.a.protho1@gmail.com>
2019-11-19 12:55:35 -0800

    test and home adjustments

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 #All TESTS FAIL, ENSURE THAT TESTS PASS BEFORE RUNNING
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+#names our url path and give it our static pages
+app = Flask(__name__, static_url_path='')
 
 #Our flask page routes :3
-#all html is bootrap reliant, bootstrap does not seem to be in any effect here.
-# ensure that css3 passes through from static page before doing any manjor changes. 
-# Then follow README page instructions. 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-#shop does not run. maybe a needed renaming in templates
 @app.route("/shop")
 def shop():
     return render_template("shop.html")
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #test driven code and tests for past code to keep website running safely
 from flask import Flask, request,render_template
 import unittest 
-from app import app, index
+from app import app, index, shop, cart, other
 
 
 class tester_Liz(unittest.TestCase):
@@ -13,26 +13,26 @@ class tester_Liz(unittest.TestCase):
     #tests our index/home page
     def tests_index(self):
             result = self.client.get('/')
-            self.assertEqual(index(), result)
+            self.assertEqual('/', result)
             self.assertEqual(index(), render_template("index.html"))
     
     #tests our shop page
     def tests_shop(self):
             result = self.client.get('/shop')
-            self.assertEqual(index(), result)
-            self.assertEqual(index(), render_template("shop.html"))
+            self.assertEqual('/shop', result)
+            self.assertEqual(shop(), render_template("shop.html"))
 
     #tests our cart page
     def tests_cart(self):
             result = self.client.get('/cart')
-            self.assertEqual(index(), result)
-            self.assertEqual(index(), render_template("cart.html"))
+            self.assertEqual('/cart', result)
+            self.assertEqual(cart(), render_template("cart.html"))
 
     #tests our other page
     def tests_other(self):
             result = self.client.get('/other')
-            self.assertEqual(index(), result)
-            self.assertEqual(index(), render_template("other.html"))
+            self.assertEqual('/other', result)
+            self.assertEqual(other(), render_template("other.html"))
 
     #look at django or somewhere else for safer programming of the admin page. we do not want 
     # outsiders gaining access to the admin page!! also remember to run $ py.test [name of file]
