8ea44f330c500a675e707936ec414fdd31f9a962
Anthony <sean.a.protho1@gmail.com>
2019-12-12 03:19:22 -0800

    jinja parent to child completion

--- Reptile_Harmony/Reptile_Harmony/urls.py
+++ Reptile_Harmony/Reptile_Harmony/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('Reptile_Harmony/', include('website.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
