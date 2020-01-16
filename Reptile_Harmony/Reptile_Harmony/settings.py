c8f60a7bf157f30938f36dd19fd562ab4b4cf625
Anthony <sean.a.protho1@gmail.com>
2020-01-15 16:59:20 -0800

    idk what this is

--- Reptile_Harmony/Reptile_Harmony/settings.py
+++ Reptile_Harmony/Reptile_Harmony/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import django_heroku
 import os
 import stripe
 if not os.getenv('PROD'):
@@ -135,6 +134,3 @@ STATICFILES_DIRS = [
 STATIC_ROOT = "/website/static/website/styles.html"
 
 STATIC_URL = '/static/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
