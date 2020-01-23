75ae0a7be464262ef54fac5f39c6e038ba45c575
Anthony <sean.a.protho1@gmail.com>
2020-01-22 19:06:51 -0800

    redesign and admin registration

--- Reptile_Harmony/website/admin.py
+++ Reptile_Harmony/website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+# pull from your models
+from website.models import *
 
-# Register your models here.
+# Model Admin represents the models presence in the admin page. This is not needed if there is
+# no customizations
+# class HomeAdmin(admin.ModelAdmin):
+#     pass
+
+admin.site.register(Home)  # HomeAdmin) #Needed when there is customizations
--- Reptile_Harmony/website/models.py
+++ Reptile_Harmony/website/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# For future refference when using django:
+# Create your models in app/models.py
+# ensure that your app is in project/settings.py installed list.
+# $python3 manage.py check
+# $python3 manage.py makemigrations
+# $python3 manage.py migrate
+# Register your models in app/admin.py
+# request your [models || database] at desired html view, in app/views.py
 
 class Home(models.Model):
     summary = models.CharField(max_length=650)
