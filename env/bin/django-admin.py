1b4781b00e86e3d4196909a8893775b061bf2f32
Anthony <sean.a.protho1@gmail.com>
2021-04-07 13:11:25 -0700

    a new start

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,7 +1,21 @@
-#!"/Users/aspin25/Coding/Personal Projects/Reptile_Harmony/env/bin/python3"
-# EASY-INSTALL-DEV-SCRIPT: 'Django==3.1','django-admin.py'
-__requires__ = 'Django==3.1'
-__import__('pkg_resources').require('Django==3.1')
-__file__ = '/Users/aspin25/Coding/Personal Projects/Reptile_Harmony/django/django/bin/django-admin.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
+#!/Users/aspin25/Coding/Personal Projects/Reptile_Harmony/env/bin/python3
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
